feb53d7dc0e41c3eb563bc563c3b2fd15a67b4b6
koefender <bernardokoefender@hotmail.com>
2021-10-16 14:10:52 -0300

    Uploaded first version of payment and added a few tests.

--- examples/payment/test/TestPayment.c
+++ examples/payment/test/TestPayment.c
@@ -20,13 +20,25 @@ TEST_TEAR_DOWN(Payment)
 
 TEST(Payment, TestPayment1)
 {
-  // All of these should pass
-  TEST_ASSERT_EQUAL(-2, payment(1,2));
-  TEST_ASSERT_EQUAL(-1, payment(2,2));
+  TEST_ASSERT_EQUAL(0, payment(0.01, (char*)"regular"));
+  TEST_ASSERT_EQUAL(0, payment(99999.00, (char*)"estudante"));
+  TEST_ASSERT_EQUAL(0, payment(89.77, (char*)"VIP"));
+  TEST_ASSERT_EQUAL(0, payment(132.57, (char*)"aposentado"));
 }
 
 TEST(Payment, TestPayment2)
 {
-  // This test will fail
-  TEST_ASSERT_EQUAL(2, foo(1,2));
+  TEST_ASSERT_EQUAL(1, payment(-0.01, (char*)"VIP"));
+  TEST_ASSERT_EQUAL(1, payment(0.00, (char*)"regular"));
+  TEST_ASSERT_EQUAL(1, payment(99999.01, (char*)"estudante"));
+  TEST_ASSERT_EQUAL(1, payment(999999.53, (char*)"VIP"));
+}
+
+TEST(Payment, TestPayment3)
+{
+  TEST_ASSERT_EQUAL(2, payment(45.30, (char*)"Aregular"));
+  TEST_ASSERT_EQUAL(2, payment(332.10, (char*)"regulara"));
+  TEST_ASSERT_EQUAL(2, payment(1132.50, (char*)""));
+  TEST_ASSERT_EQUAL(2, payment(25000.30, (char*)"idfgjapeçiokmgvalfkgjari**jgk$$/mfagmçalcbma@ekpotŕitoṕowekffla,mçvak$dnçglasngaçv,#;.mafga4p3353p3kr-"));
+  TEST_ASSERT_EQUAL(2, payment(33.00, NULL));
 }
